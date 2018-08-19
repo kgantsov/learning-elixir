29537d26a782cda5e3293e950e0e9f350732d293
Kostiantyn Hantsov <k.gantsov@gmail.com>
2018-08-19 19:04:47 +0200

    Input validation for rock_scissors_paper game

--- rock_scissors_paper/lib/main.ex
+++ rock_scissors_paper/lib/main.ex
@@ -4,18 +4,27 @@ defmodule RockScissorsPaper.CLI do
   end
 
   def move() do
-    player = IO.gets("Rock, Paper, Scissors?\nYour move is: ")
+    player = get_player_move()
+
+    if !Enum.member?([:Rock, :Paper, :Scisors], player) do
+      IO.puts("You should type either: Rock, Paper or Scissors")
+      move()
+    else
+      ai = RockScissorsPaper.ai_make_move()
+
+      winner = RockScissorsPaper.get_winner(player, ai)
+      |> Atom.to_string()
+      |> String.capitalize()
+
+      IO.puts("AI's move is: #{ai}")
+      IO.puts("Winner is: #{winner}")
+    end
+  end
+
+  def get_player_move() do
+    IO.gets("Rock, Paper, Scissors?\nYour move is: ")
     |> String.capitalize()
     |> String.trim()
     |> String.to_atom()
-
-    ai = RockScissorsPaper.ai_make_move()
-
-    winner = RockScissorsPaper.get_winner(player, ai)
-    |> Atom.to_string()
-    |> String.capitalize()
-
-    IO.puts("AI's move is: #{ai}")
-    IO.puts("Winner is: #{winner}")
   end
 end
