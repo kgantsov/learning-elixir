8266261a931924cf84f6dce272c4c01b0c58876e
Kostiantyn Hantsov <k.gantsov@gmail.com>
2018-08-22 20:01:12 +0200

    Add some tests for rock paper scisors

--- rock_scissors_paper/test/rock_scissors_paper_test.exs
+++ rock_scissors_paper/test/rock_scissors_paper_test.exs
@@ -6,8 +6,18 @@ defmodule RockScissorsPaperTest do
     assert RockScissorsPaper.hello() == :world
   end
 
-  test "greets the world" do
-    assert RockScissorsPaper.hello() == :world
+  test "get winner" do
+    assert RockScissorsPaper.get_winner(:Paper, :Rock) == :you
+    assert RockScissorsPaper.get_winner(:Rock, :Scisors) == :you
+    assert RockScissorsPaper.get_winner(:Scisors, :Paper) == :you
+
+    assert RockScissorsPaper.get_winner(:Rock, :Paper) == :ai
+    assert RockScissorsPaper.get_winner(:Scisors, :Rock) == :ai
+    assert RockScissorsPaper.get_winner(:Paper, :Scisors) == :ai
+
+    assert RockScissorsPaper.get_winner(:Rock, :Rock) == :tie
+    assert RockScissorsPaper.get_winner(:Scisors, :Scisors) == :tie
+    assert RockScissorsPaper.get_winner(:Paper, :Paper) == :tie
     # for p1 <- [:paper, :rock, :scisors], p2 <- [:paper, :rock, :scisors], into: %{} do {"#{p1} = #{p2}", RockScissorsPaper.get_winner(p1, p2)} end
   end
 end
